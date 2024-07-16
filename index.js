834ba2c16f008c21cff1fa6f5de852740319024e
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-07-16 15:20:41 +0300

    vazifa

---
+++ index.js
@@ -0,0 +1,111 @@
+// Qoshimcha vazifa  misollari
+
+
+// 1-misol
+// let sm = +prompt("uzunlikni sm kiriting");
+// let result = Math.trunc(sm / 100);
+
+// console.log(result)
+
+
+
+
+
+// 2-misol
+
+// let m = +prompt("Og`irlikni kg kiriting")
+// let result = Math.trunc(m / 1000)
+
+// console.log(result)
+
+
+
+
+
+// 3-misol
+
+// let bayt = +prompt("Faylni kilobaytlarda kiriting")
+// let result = Math.trunc(bayt/1024)
+
+// console.log(result)
+
+
+
+// 6-misol
+
+// let num = +prompt("2 xonali son kiriting");
+// let birlar = num %10;
+// let onlar = Math.trunc(num / 10 );
+
+// console.log(onlar, birlar) 
+
+
+
+// 7-misol
+
+// let num = +prompt("2 xonali son kiriting");
+// let birlar = num %10;
+// let onlar = Math.trunc(num / 10 );
+// let result = birlar + onlar;
+// console.log(result)     
+
+// 11-misol
+
+
+
+// let num = +prompt("3 xonali sonlarni raqamlar yigindisi");
+// let birlar = num % 10;
+// let onlar = Math.trunc((num/10)%10);
+// let yuzlik = Math.trunc(num / 100);
+
+
+// console.log(yuzlik + onlar + birlar)  
+
+// 10-misol
+
+// let num = +prompt("3 xonali sonlarni raqamlar yigindisi");
+// let birlar = num % 10;
+// let onlar = Math.trunc((num/10)%10);
+// let yuzlik = Math.trunc(num / 100);
+
+
+// console.log(birlar, onlar)
+
+
+
+
+// 12-misol
+
+// let num = +prompt("3 xonali sonlarni raqamlar yigindisi");
+// let birlar = num % 10;
+// let onlar = Math.trunc((num/10)%10);
+// let yuzlik = Math.trunc(num / 100);
+
+
+// console.log(birlar, onlar, yuzlik)
+
+
+// 13-misol
+
+// let num = +prompt("3 xonali sonlarni raqamlar yigindisi");
+// let birlar = num % 10;
+// let onlar = Math.trunc((num/10)%10);
+// let yuzlik = Math.trunc(num / 100);
+
+
+// console.log(birlar, yuzlik, onlar)   
+
+
+
+
+
+
+// Asosiy masalalar
+
+// 1-misol
+
+// let num = +prompt("Sonni Kiriting");
+// let num2 = Math.floor(num / 2);
+// let result = num % 2;
+// console.log("Butun qismi:", num2);
+// console.log("Qoldiq:", result)
